d81416cada5e512824bb80f5088f95a876b1e7ef
Richard Morris <richmorris55@yahoo.com>
2023-02-09 07:41:39 -0800

    SetOneInitialCommit

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -4,6 +4,8 @@ namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
+        //Completed Challenge
+
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if(num1 == num2)
@@ -18,32 +20,39 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return (minuend - subtrahend);
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return (number1 + number2);
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 <= number2 ? number1 : number2;
+                    
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return(factor1 * factor2);
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson=="")
+            {
+                return "Hello!";
+            }
+
+            return "Hello, " + nameOfPerson + "!";
+            
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
