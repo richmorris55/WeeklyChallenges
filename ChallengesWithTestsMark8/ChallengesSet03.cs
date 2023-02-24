8e8d4803168ca91514acfffd93d362312ac956c2
Richard Morris <richmorris55@yahoo.com>
2023-02-24 07:12:10 -0800

    WeekThreeSet

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,146 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < vals.Length; i++ ) 
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+
+                
+            }
+            return false;
+                
+            
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+          
+            if (numbers == null || !numbers.Any())
+            {
+                return false;
+            }
+
+            List<int> oddNumbersList = new List<int>();
+            var numberArray = numbers.ToArray();
+
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                var number = numberArray[i];
+                if (number % 2 != 0)
+                {
+                    oddNumbersList.Add(number);
+                }
+            };
+
+            var sum = oddNumbersList.Sum();
+
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+
+            //var linqSum = numbers.Select(x => x).Where(x => x % 2 != 0).Sum();
+
+            //return linqSum % 2 != 0 ? true :false;
+
+            return false; 
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool UpperCase = false;
+            bool LowerCase = false;
+            bool Number = false;
+
+            foreach (char x in password)
+            {
+                if (char.IsUpper(x))
+                {
+                    UpperCase = true;
+                }
+
+                else if (char.IsLower(x))
+                {
+                     LowerCase = true;
+                }
+
+                else if (char.IsDigit(x))
+                {
+                    Number = true;
+                }
+
+                
+            }
+
+            return UpperCase && LowerCase && Number;
+
+
+
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0]; 
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char lastLetter = val[val.Length - 1];
+
+            return lastLetter;
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
-        }
+           
+            if(divisor == 0)
+            {
+                return 0;
+            }
+
+            return dividend / divisor;
+        }   
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            int first = nums[0];
+            int last = nums[nums.Length - 1];
+            return last - first;
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] odds = new int[50];
+            int index = 0;
+
+            for (int i = 1; i < 100; i += 2)
+            {
+                odds[index] = i;
+                index++;
+            }
+
+            return odds;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null)
+            {
+                
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != null)
+                {
+                    words[i] = words[i].ToUpper();
+                }
+            }
+
+            
         }
     }
 }
